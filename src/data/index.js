599d3725fa71d34079a85f8710ce7977838d34df
lenilsonxavier-dev <lenilsonxavier@gmail.com>
2026-05-18 18:58:21 -0300

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,61 @@
+import adjetivos from "./adjetivos.json";
 import animaisFazenda from "./animais_fazenda.json";
-import comidas from "./comidas.json";
+import animaisMarinhos from "./animais_marinhos.json";
+import animaisSelvagens from "./animais_selvagens.json";
+import aniversarioExpressoes from "./aniversario_expressoes.json";
 import clima from "./clima.json";
+import comidas from "./comidas.json";
+import cores from "./cores.json";
+import cumprimentos from "./cumprimentos.json";
+import curiosidadesLinguaInglesa from "./curiosidades_lingua_inglesa.json";
+import datasComemorativasIngles from "./datas_cdatas_comemorativas_ingles.json";
+import diasDaSemana from "./dias_da_semana.json";
+import estacoes from "./estacoes.json";
+import fasesDaVida from "./fases_da_vida.json";
+import glossario from "./glossario.json";
+import halloween from "./halloween.json";
+import horas from "./horas.json";
+import materiasEscolares from "./materias_escolares.json";
+import mesesDoAno from "./meses_do_ano.json";
+import numerosOrdinais from "./numeros_ordinais.json";
+import oQueELinguaInglesa from "./o_que_e_lingua_inglesa.json";
+import objetosEscolares from "./objetos_escolares.json";
+import opostos from "./opostos.json";
+import passaros from "./passaros.json";
+import pequenosDialogos from "./pequenos_dialogos.json";
+import rotinas from "./rotinas.json";
+import roupas from "./roupas.json";
+import sinonimos from "./sinonimos.json";
+import substantivos from "./substantivos.json";
 
 export const knowledgeBase = {
+  ...adjetivos,
   ...animaisFazenda,
+  ...animaisMarinhos,
+  ...animaisSelvagens,
+  ...aniversarioExpressoes,
+  ...clima,
   ...comidas,
-  ...clima
+  ...cores,
+  ...cumprimentos,
+  ...curiosidadesLinguaInglesa,
+  ...datasComemorativasIngles,
+  ...diasDaSemana,
+  ...estacoes,
+  ...fasesDaVida,
+  ...glossario,
+  ...halloween,
+  ...horas,
+  ...materiasEscolares,
+  ...mesesDoAno,
+  ...numerosOrdinais,
+  ...oQueELinguaInglesa,
+  ...objetosEscolares,
+  ...opostos,
+  ...passaros,
+  ...pequenosDialogos,
+  ...rotinas,
+  ...roupas,
+  ...sinonimos,
+  ...substantivos
 };
